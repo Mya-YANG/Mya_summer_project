b68c82451eedebf64ab1ec384baf1e1dd8c5fb35
Mya <zd21874@bristol.ac.uk>
2022-12-12 17:40:13 +0800

    some update

---
+++ 20pic_tiny dataset /V5/main.py
@@ -0,0 +1,23 @@
+import json
+import os
+
+if __name__ == '__main__':
+
+    if not os.path.exists("mega_json"):
+        os.mkdir("mega_json")
+
+    with open("20pic.json", "r") as source_json_fp:
+        source_json = json.load(source_json_fp)
+
+        for image in source_json['images']:
+            new_json = {'images': [image]}
+            for key in source_json:
+                if key == "images":
+                    continue
+                new_json[key] = source_json[key]
+
+            filename = str(image['file']).split('.')[0]
+            print("Writing to {}.json.".format(filename))
+
+            with open("mega_json/{}.json".format(filename), "w") as new_json_fp:
+                json.dump(new_json, new_json_fp, indent=1)
